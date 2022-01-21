c4a5f565476cbcba447ce879775ab6a995365863
GrantBryan28 <grantbryan28@gmail.com>
2022-01-21 18:54:59 -0500

    Update twitterbot.py

--- twitterbot.py
+++ twitterbot.py
@@ -2,3 +2,8 @@ import tweepy
 
 auth = tweepy.OAuthHandler("consumer_key", "consumer_secret")
 auth.set_access_token("access_token", "access_token_secret")
+
+api = tweepy.API(auth)
+
+api.update_status("Hello Tweepy")
+
